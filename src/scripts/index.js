a8aeec756b96804465dce2c31d5b6a85ad71d7a8
Louison Gitzinger <gitzingerlouison@gmail.com>
2019-06-24 13:48:37 +0100

    up

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,31 +5,58 @@ let articleId = 0;
 
 setArticle(articleId);
 
+
+document.querySelector('#left-arrow').addEventListener('click', () => {
+  const oeuvres = document.querySelector('#oeuvres').children;
+  const oeuvresSize = oeuvres.length;
+  articleId = articleId - 1;
+  if (articleId < 0) {
+    articleId = oeuvresSize - 1;
+  }
+  setArticle(articleId);
+});
+document.querySelector('#right-arrow').addEventListener('click', () => {
+  const oeuvres = document.querySelector('#oeuvres').children;
+  const oeuvresSize = oeuvres.length;
+  articleId++;
+  if (articleId >= oeuvresSize) {
+    articleId = 0;
+  }
+  setArticle(articleId);
+});
+
 function setOnClickListener() {
-  document.querySelector('.article-img').addEventListener('click', () => {
+  document.querySelector('picture').addEventListener('click', () => {
     const oeuvres = document.querySelector('#oeuvres').children;
     const oeuvresSize = oeuvres.length;
     articleId++;
     if (articleId >= oeuvresSize) {
       articleId = 0;
     }
-    console.log(articleId);
     setArticle(articleId);
   });
 }
 
 function setArticle(articleId) {
-  console.log(articleId);
   const oeuvres = document.querySelector('#oeuvres').children;
   const articleContainer = document.querySelector('#article-container');
+  const legendContainer = articleContainer.querySelector('.legend-container');
   const currentArticle = oeuvres[articleId];
-  articleContainer.innerHTML = currentArticle.innerHTML;
-  const legendContent = articleContainer.querySelector('.legend-content');
+  legendContainer.innerHTML = currentArticle.querySelector('.legend').innerHTML;
+  // articleContainer.innerHTML = currentArticle.innerHTML;
+  const legendContent = legendContainer.querySelector('.legend-content');
   let legendText = currentArticle.querySelector(
       'lang[code=' + navigator.language + ']');
   if (legendText == null) {
     legendText = currentArticle.querySelector('lang[code=fr-FR]');
   }
   legendContent.innerHTML = legendText.querySelector('p').innerHTML;
+  const pictureContainer = articleContainer.querySelector('.picture-container');
+  const currentPic = pictureContainer.querySelector('picture');
+  if (currentPic != null) {
+    pictureContainer.removeChild(currentPic);
+  }
+  const picture = currentArticle.querySelector('picture').cloneNode(true);
+  pictureContainer.insertBefore(picture, pictureContainer.firstChild);
   setOnClickListener();
 }
