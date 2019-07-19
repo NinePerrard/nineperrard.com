f2205466b4abcd29f20afc74b273f14dc67bf13e
Louison Gitzinger <gitzingerlouison@gmail.com>
2019-07-19 17:42:35 +0200

    up

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -2,52 +2,77 @@ const Path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const fs = require('fs')
+
+function generateHtmlPlugins (templateDir, lang) {
+  const templateFiles = fs.readdirSync(Path.resolve(__dirname, templateDir + '/' + lang));
+  return templateFiles.map(item => {
+    // Split names and extension
+    const parts = item.split('.');
+    const name = parts[0];
+    const extension = parts[1];
+    const directory = name;
+    return new HtmlWebpackPlugin({
+      filename: `${lang}/${directory}/index.html`,
+      template: Path.resolve(__dirname, `${templateDir}/${lang}/${name}.${extension}`)
+    });
+  });
+}
+
+// We will call the function like this:
+const htmlPluginsEn = generateHtmlPlugins('../src/templates/views', 'en');
+const htmlPluginsFr = generateHtmlPlugins('../src/templates/views', 'fr');
 
 module.exports = {
   entry: {
-    app: Path.resolve(__dirname, '../src/scripts/index.js')
+    app: Path.resolve(__dirname, '../src/scripts/index.js'),
   },
   output: {
     path: Path.join(__dirname, '../docs'),
-    filename: 'js/[name].js'
+    filename: 'js/[name].js',
   },
   optimization: {
     splitChunks: {
       chunks: 'all',
-      name: false
-    }
+      name: false,
+    },
   },
   plugins: [
     new CleanWebpackPlugin(),
     new CopyWebpackPlugin([
-      { from: Path.resolve(__dirname, '../public'), to: 'public' },
-      { from: Path.resolve(__dirname, '../assets'), to: 'assets' }
+      { from: Path.resolve(__dirname, '../public'), to: '.' },
+      { from: Path.resolve(__dirname, '../assets'), to: 'assets' },
     ]),
     new HtmlWebpackPlugin({
-      template: Path.resolve(__dirname, '../src/index.html'),
-    })
-  ],
+      filename: 'index.html',
+      template: Path.resolve(__dirname, '../src/templates/views/en/dismissed_narratives.pug'),
+    }),
+  ].concat(htmlPluginsEn).concat(htmlPluginsFr),
   resolve: {
     alias: {
-      '~': Path.resolve(__dirname, '../src')
-    }
+      '~': Path.resolve(__dirname, '../src'),
+    },
   },
   module: {
     rules: [
       {
+        test: /\.pug$/,
+        use: ['pug-loader'],
+      },
+      {
         test: /\.mjs$/,
         include: /node_modules/,
-        type: 'javascript/auto'
+        type: 'javascript/auto',
       },
       {
         test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
         use: {
           loader: 'file-loader',
           options: {
-            name: '[path][name].[ext]'
-          }
-        }
+            name: '[path][name].[ext]',
+          },
+        },
       },
-    ]
-  }
+    ],
+  },
 };
